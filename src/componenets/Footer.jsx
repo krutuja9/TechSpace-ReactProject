d43167af56b40eee0651a293ddabf3927ff7969d
krutuja9 <krutuja609@gmail.com>
2023-08-30 13:56:39 +0530

    First Project- Service  website

---
+++ src/componenets/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow us</h5>
+
+        <div>
+
+          <a href="https://github.com/krutuja9" target={"blank"}>
+            Youtube</a>
+          <a href="https://github.com/krutuja9" target={"blank"}>
+            Instagram</a>
+          <a href="https://github.com/krutuja9" target={"blank"}>Github</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
